9731eaf911d0f30444a355bd3b86e8cf47f11a95
house <spthouse16@gmail.com>
2025-12-11 11:16:49 -0600

    assort py

--- src/tools/update_gui_py.ps1
+++ src/tools/update_gui_py.ps1
@@ -3,3 +3,4 @@ pyuic6 -o gui_about.py gui_about.ui
 pyuic6 -o gui_updates.py gui_updates.ui
 pyuic6 -o gui_quests.py gui_quests.ui
 pyuic6 -o gui_tasks.py gui_tasks.ui
+pyuic6 -o gui_assort.py gui_assort.ui
